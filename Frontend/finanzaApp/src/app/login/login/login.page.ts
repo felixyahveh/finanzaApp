dc0592023bfa6072e14253f70a2b5432c5962814
felixyahveh <felixyahveh@gmail.com>
2024-05-14 22:50:33 -0600

    add(frontend): login page

--- Frontend/finanzaApp/src/app/login/login/login.page.ts
+++ Frontend/finanzaApp/src/app/login/login/login.page.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  public imgLogo:string = 'https://static.vecteezy.com/system/resources/previews/014/611/290/original/financial-piggy-bank-ideas-for-saving-money-for-the-future-png.png'
 
   constructor() { }
 
